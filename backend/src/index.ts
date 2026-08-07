ea42aba42dc709c90e90e744d0b8167f23a109d0
Krishna-Gupta17 <2024021228@mmmut.ac.in>
2026-08-07 14:49:30 +0530

    ts fixed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import type { Request, Response } from 'express';
+// import type { Request, Response } from 'express';
 import type { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import fs from 'fs';
